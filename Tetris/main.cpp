c48cb384ac9f94b50196d60da3818e3723cccd5e
Moises Limon <moises_limon@Moisess-MacBook-Air.local>
2021-04-09 14:30:55 -0700

    init commit - moises

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     //Test commit Michael
     //Test commit Alberto
-
+    //Test commit Moises
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
